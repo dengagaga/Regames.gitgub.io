e38bcb3baf0134c51bee064a900a03314d6513ea
dengagaga <134398955+dengagaga@users.noreply.github.com>
2024-02-18 12:12:06 +0300

    Add files via upload

--- js/swiperHumansOne.js
+++ js/swiperHumansOne.js
@@ -1,16 +1,18 @@
 var swiper = new Swiper(".mySwiper", {
-
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
+
+      
+
   });
   var swiper2 = new Swiper(".mySwiper2", {
-
+    
     spaceBetween: 10,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-btn-next",
+      prevEl: ".swiper-btn-prev",
     },
     thumbs: {
       swiper: swiper,
